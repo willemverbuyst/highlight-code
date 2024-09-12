b9885a671db0dc236a90b82c36fdc77d28c8477b
Willem Verbuyst <willemverbuyst@gmail.com>
2024-09-12 04:07:50 +0200

    Highlight attributes

--- src/highlight.ts
+++ src/highlight.ts
@@ -1,13 +1,13 @@
 const HTML_STRING = `
   <body>
     <header>
-      <h1>Title</h1>
+      <h1 class="title">Title</h1>
     </header>
     <main>
       <section>
         <p>This is a paragraph!</p>
       </section>
-      <section>
+      <section id="list">
         <ul>
           <li>List item 1</li>
           <li>List item 2</li>
@@ -20,7 +20,14 @@ const HTML_STRING = `
 export function highlightTags() {
   const output = HTML_STRING.replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/(&lt;\/?[\w-]+&gt;)/g, '<span class="html-tag">$1</span>');
+    .replace(
+      /(&lt;\/?)([\w-]+)(&gt;)/g,
+      '<span class="html-symbol">$1</span><span class="html-element">$2</span><span class="html-symbol">$3</span>'
+    )
+    .replace(
+      /(&lt;)([\w-]+\s)(class|id)(=")([a-z-]+)(")(&gt;)/g,
+      '<span class="html-symbol">$1</span><span class="html-element">$2</span><span class="html-attribute">$3</span><span class="html-symbol">$4</span><span class="html-attribute">$5</span><span class="html-symbol">$6</span><span class="html-symbol">$7</span>'
+    );
 
   const textField = document.querySelector<HTMLElement>("#html-text");
   if (textField) textField.innerHTML = output;
